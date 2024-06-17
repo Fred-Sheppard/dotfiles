f24dd0651a828812d773339415ea69c0d10b3101
Fred Sheppard <frederickjssheppard@gmail.com>
2024-06-17 02:13:00 +0100

    Install neovim using tar, was apt

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install -y $(cat apt-requirements.txt)
 
+# neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
 # rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
@@ -32,3 +37,4 @@ ln -fs $HOME/dotfiles/starship.toml $HOME/.config/starship.toml
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # zsh-vi-mode
 git clone https://github.com/jeffreytse/zsh-vi-mode $HOME/.oh-my-zsh/custom/plugins/zsh-vi-mode
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
