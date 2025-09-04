de46abfa3ebb2b1a6b31c0b409f2561a3ffbbd72
fred <frederickjssheppard@gmail.com>
2025-09-04 21:17:53 +0100

    nvim: Arrow keys in autocomplete

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -4,7 +4,9 @@ return {
         keymap = {
             preset = "none",
             ["<Tab>"] = { "select_next", "fallback" },
+            ["<Down>"] = { "select_next", "fallback" },
             ["<S-Tab>"] = { "select_prev", "fallback" },
+            ["<Up>"] = { "select_prev", "fallback" },
             ["<CR>"] = { "select_and_accept", "fallback" },
         },
     },
