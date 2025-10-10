ab8566833ebaca3ad7d272610e09e0d2024d71da
fred <frederickjssheppard@gmail.com>
2025-10-10 02:34:37 +0100

    nvim: typst: use typstyle as formatter

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -14,6 +14,11 @@ return {
             },
           },
         },
+        tinymist = {
+          settings = {
+            formatterMode = "typstyle",
+          },
+        },
       },
     },
   },
