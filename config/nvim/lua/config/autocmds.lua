e773226270dca0b45a899fc182fe8d60bd2df779
Fred Sheppard <frederickjssheppard@gmail.com>
2025-10-26 18:21:11 +0000

    nvim: Update typst config

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto-change to the file's directory on open
 vim.api.nvim_create_autocmd("BufEnter", {
@@ -44,49 +43,4 @@ vim.filetype.add({
   },
 })
 
--- Add custom command for Typst export
-vim.api.nvim_create_user_command("ExportTypst", function()
-  local buf = vim.api.nvim_get_current_buf()
-  local filepath = vim.api.nvim_buf_get_name(buf)
-
-  -- Check if buffer has a file
-  if filepath == "" then
-    vim.notify("No file in current buffer", vim.log.levels.ERROR)
-    return
-  end
-
-  -- Check if it's a .typ file
-  if not filepath:match("%.typ$") then
-    vim.notify("Current file is not a Typst file (.typ)", vim.log.levels.WARN)
-    return
-  end
-
-  -- Extract directory and filename
-  local dir = vim.fn.fnamemodify(filepath, ":h")
-  local filename = vim.fn.fnamemodify(filepath, ":t")
-  local basename = vim.fn.fnamemodify(filepath, ":t:r")
-
-  -- Build the command
-  local cmd = string.format(
-    "cd %s && mkdir -p out && typst compile %s out/%s.pdf && open out/%s.pdf",
-    vim.fn.shellescape(dir),
-    vim.fn.shellescape(filename),
-    basename,
-    basename
-  )
-
-  -- Execute the command
-  vim.notify("Compiling " .. filename .. "...", vim.log.levels.INFO)
-  vim.fn.system(cmd)
-
-  -- Check for errors
-  if vim.v.shell_error ~= 0 then
-    vim.notify("Typst compilation failed", vim.log.levels.ERROR)
-  else
-    vim.notify("PDF exported to out/" .. basename .. ".pdf", vim.log.levels.INFO)
-  end
-end, {
-  desc = "Export current Typst file to PDF",
-})
-
-vim.opt.mouse = ""
+-- vim.opt.mouse = ""
