58852f3771a7c45cebe6c8cb24bbb4f35d17154c
Fred Sheppard <frederickjssheppard@gmail.com>
2026-04-13 11:19:54 +0100

    zsh/openchip: Update openchip device rc

--- config/device/openchip-wsl.zsh
+++ config/device/openchip-wsl.zsh
@@ -1,4 +1,8 @@
 export PATH="/mnt/c/Users/frederick.sheppard/scoop/shims:$PATH"
+alias open="explorer.exe"
+alias copy="clip.exe"
 
-
-eval "$(zellij setup --generate-auto-start zsh)"
+# Only run zellij if not connected to JetBrains IDE or VS Code
+if [[ -z "$TERMINAL_EMULATOR" && -z "$TERM_PROGRAM" ]]; then
+    eval "$(zellij setup --generate-auto-start zsh)"
+fi
