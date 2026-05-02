aa8f118c363431e79d590dc7cafe755e671f21d4
Fred Sheppard <frederickjssheppard@gmail.com>
2026-05-02 23:40:33 +0100

    zsh/openchip: Add explorer alias

--- config/device/openchip-wsl.zsh
+++ config/device/openchip-wsl.zsh
@@ -1,6 +1,7 @@
 export PATH="/mnt/c/Users/frederick.sheppard/scoop/shims:$PATH"
 alias open="explorer.exe"
 alias copy="clip.exe"
+alias open="explorer.exe"
 
 # Only run zellij if not connected to JetBrains IDE or VS Code
 if [[ -z "$TERMINAL_EMULATOR" && -z "$TERM_PROGRAM" ]]; then
