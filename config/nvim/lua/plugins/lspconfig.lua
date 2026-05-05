75d4156c9d80a778042281cdb6298d904ef039aa
Fred Sheppard <frederickjssheppard@gmail.com>
2026-05-05 13:41:27 +0100

    nvim/harper-ls: Fix enable harper for typst files

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       servers = {
         harper_ls = {
-          filetypes = { "typ" },
+          filetypes = { "typst" },
           settings = {
             ["harper-ls"] = {
               linters = {
